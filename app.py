6a60ddf06c252c118ed47ec872d443343488c9b5
rudannimili-99 <rudanimili1118@gmail.com>
2026-03-26 14:09:31 +0530

    Refactor app.py to use environment variable for port

--- app.py
+++ app.py
@@ -21,5 +21,8 @@ def apkonic():
         "clientIp": get_client_ip()
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True,port=3000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
