0c6a77eaf90bca2d3f281c17decc00d1b3610ae9
rudannimili-99 <rudanimili1118@gmail.com>
2026-03-29 21:20:41 +0530

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 app = Flask(__name__, template_folder="templates", static_folder="static")
 # MongoDB Atlas connection
 client = MongoClient("mongodb+srv://rudanimili1118_db_user:MiliRudani1234@cluster0.vweh5lh.mongodb.net/?retryWrites=true&w=majority&serverSelectionTimeoutMS=5000")
-db = client["apkonic_db"]
+db = client["apkonic"]
 collection = db["users"]
 try:
     client.admin.command('ping')
